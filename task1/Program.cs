016b046df11806fa78b000dd37f4e7d95fec4404
JarLaX99 <vanek10699@gmail.com>
2023-07-04 19:29:33 +0300

    семинар 7 , задача 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿//  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void main()
+{
+    int row = ReadInt("Введите кол-во строк массива : ");
+    int col = ReadInt("Введите кол-во столбцов массива : ");
+    double[,] matrix = FullMatrix(row, col);
+    PrintMatrix(matrix);
+}
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double[,] FullMatrix(int row = 5, int col = 5, int leftRange = -10, int rightRange = 10)
+{
+    double[,] tempMatrix = new double[row, col];
+    Random rand = new Random();
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            tempMatrix[i, j] = Math.Round(rand.Next(leftRange, rightRange + 1) + rand.NextDouble(), 2);
+        }
+    }
+    return tempMatrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+main();
